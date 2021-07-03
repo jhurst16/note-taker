6f61cd350192bd4c438e3e97539c26c19ad3884d
jhurst16 <jeremyhurst16@gmail.com>
2021-07-03 11:47:23 -0400

    updated package.json dependencies

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,3 +4,16 @@ const path = require("path")
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"))
 })
+
+
+
+
+
+
+
+
+
+
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+})
